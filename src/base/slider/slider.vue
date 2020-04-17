5a7a2fb15dc37aef5ab9f582031782eefd320dd6
Hepca <wangqiao@qiaoqiaodeMacBook-Pro.local>
2020-04-17 21:21:45 +0800

    componment slider

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="slider" ref="slider">
+      <div class="slider-group" ref="sliderGroup">
+          <slot></slot>
+          <div class="dots"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+import { addClass } from 'common/js/dom'
+export default {
+    name: 'slider',
+    props: {
+        loop: {
+            type: Boolean,
+            default: true
+        },
+        autoPlay: {
+            type: Boolean,
+            default: true
+        },
+        interval: {
+            type: Number,
+            default: 4000
+        }
+    },
+    methods: {
+        _setSliderWidth() {
+            this.children = this.$refs.sliderGroup.children
+            let width = 0
+            let siliderWidth = this.$refs.slider.clientWidth
+            for (let i = 0; i < this.children.length; i++) {
+                let child = this.children[i]
+                addClass(child, 'slider-item')
+                child.style.width = siliderWidth+ 'px'
+                width += siliderWidth
+            }
+            if (this.loop) {
+                width += 2 * siliderWidth
+            }
+            this.$refs.siliderWidth.style.width = width + 'px'
+        },
+        _initSlider() {
+            this.slider = new BScroll(this.$refs.slider, {
+                scollX: true,
+                scollY: false,
+                momentum: false,
+                snap: true,
+                snapLoop: this.loop,
+                snapThreshold: 0.3,
+                snapSpeed: 400,
+                click: true
+            })
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this._initSlider()
+        },20)
+    }
+}
+</script>
+
+<style scope lang="stylus" rel="stylesheet/stylus">
+    @import '~common/stylus/variable'
+    .slider
+        min-height: 1px
+        .slider-group
+            position: relative
+            overflow: hidden
+            white-space: nowrap
+            .slider-item
+                float:left
+                box-sizing: border-box
+                overflow: hidden
+                text-align: center
+                a
+                    display: block
+                    width: 100%
+                    overflow: hidden
+                    text-decoration: none 
+                img 
+                    display: block
+                    width: 100%
+            .dots
+                position: absolute
+                right: 0
+                left: 0
+                bottom: 12px
+                text-align: center
+                font-size: 0
+                .dot
+                    display: inline-block
+                    margin: 0 4px
+                    width: 8px
+                    height: 8px
+                    border-radius: 50%
+                    background: $color-text-1
+                    &.active
+                        width: 20px
+                        border-radius: 5px
+                        background: $color-text-11
+</style>
