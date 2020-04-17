64abd4b0e762b489efea700ce624de05d9185186
wangqiao <wangqiao@91160.com>
2020-04-17 14:02:27 +0800

    all configuation

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export const commonParams = {
+    g_tk: 5381,
+    inCharset: 'utf-8',
+    outCharset: 'utf-8',
+    notice: 0,
+    format: 'jsonp'
+}
+
+export const options = {
+    param: 'jsonCallback'
+}
+const ERR_OK = 0
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="recommend">
+      <div class="recommend-content">
+          <div class="slider-wrapper"></div>
+          <div class="recommend-list">
+              <h1 class="list-title">热门歌单推荐</h1>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import '~common/stylus/variable'
+    .recommend 
+        position: fixed
+        width: 100%
+        top: 88px
+        bottom: 0
+        .recommend-content 
+            height: 100%
+            overflow: hidden
+            .slider-wrapper 
+                
+</style>
